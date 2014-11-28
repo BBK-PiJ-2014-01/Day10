95ac9e405c5195eaec8d65ba2c5469839e8a3e1d
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-11-28 22:03:06 +0000

    Initial commit

---
+++ src/Lecturer.java
@@ -0,0 +1,10 @@
+// Lecturer.java
+/**
+ * A lecturer has both teaching and research responsibilities
+ */
+public class Lecturer extends Teacher {
+
+    public void doResearch(String topic) {
+        System.out.println("Doing research on: " + topic);
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Pierre on 28/11/2014.
+ */
+public class Teacher {
+
+    private String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void teach(String lessonName) {
+        System.out.println("Teaching lesson: " + lessonName);
+    }
+}
