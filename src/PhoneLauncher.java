b61419a7f14e79c09df8caba64ff215c0a3410b2
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-11-28 21:53:09 +0000

    Initial commit

---
+++ src/PhoneLauncher.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Pierre on 27/11/2014.
+ */
+public class PhoneLauncher {
+    public static void main(String[] args) {
+        PhoneLauncher launcher = new PhoneLauncher();
+        launcher.launch();
+    }
+
+    public void launch() {
+        SmartPhone sp = new SmartPhone("Nokia");
+        sp.call("0203 128 2441");
+        sp.call("0203 128 2321");
+        sp.call("00 203 128 2222");
+        sp.call("0203 128 2981");
+        sp.ringAlarm("Time to get up!");
+        sp.playGame("Hay Day");
+        sp.browseWeb("www.aaa.com");
+        String position = sp.findPosition();
+        System.out.println(position);
+        sp.printLastNumbers();
+        System.out.println("Brand: " + sp.getBrand());
+    }
+}
---
+++ src/RestrictedSmartPhone.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Pierre on 28/11/2014.
+ */
+public class RestrictedSmartPhone extends SmartPhone {
+
+}
