50036ac85c4839c4526e3d09c01532fe77453a9f
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-23 18:02:31 +0000

    Initial commit

---
+++ src/MusicalInstrument.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Pierre on 23/12/2014.
+ */
+public class MusicalInstrument {
+    public void play() {
+        System.out.println("Playing...");
+    }
+}
---
+++ src/WoodenObject.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Pierre on 23/12/2014.
+ */
+public class WoodenObject {
+    public void burn() {
+        System.out.println("Burning...");
+    }
+}
